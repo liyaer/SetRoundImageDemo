38f867f655a94b08655cf623a5cd377175145ec9
DuWenLiang <739285514@qq.com>
2017-11-30 16:10:56 +0800

    Initial Commit

---
+++ SetRoundImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SetRoundImageDemo
+//
+//  Created by 杜文亮 on 2017/11/30.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
